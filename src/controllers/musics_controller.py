2da496671e4d5f21410935b568788f94ee3bae84
uelionobre <uelionobre@hotmail.com>
2023-12-23 19:48:42 -0300

    Implementa uma funcao privada para obter todas as musicas

--- src/controllers/musics_controller.py
+++ src/controllers/musics_controller.py
@@ -1,4 +1,13 @@
 from flask import Blueprint
 
+from models.music_model import MusicModel
+
 # Definindo o controller para musicas
 musics_controller = Blueprint("musics", __name__)
+
+
+# ----------
+# Funções protegidas
+def _get_all_musics():
+    musics = MusicModel.find()
+    return [music.to_dict() for music in musics]
