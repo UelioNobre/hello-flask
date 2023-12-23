f24f884881909c3e7429107d9d0b13d551876bf8
uelionobre <uelionobre@hotmail.com>
2023-12-23 19:18:00 -0300

    Remove conteudo estatico de jokes em app

--- src/app.py
+++ src/app.py
@@ -1,28 +1,10 @@
 from os import environ
 from waitress import serve
-from flask import Flask, jsonify
-import requests
-import random
+from flask import Flask
+from controllers.jokes_controller import jokes_controller
 
 app = Flask(__name__)
-
-joke_list = [
-    "Por que o bombeiro não gosta de andar? <br> Porque ele socorre.",
-    "Sabe como chama a sorveteria do Michel Teló? <br> Ice te Pego.",
-    "Por que o espanador não luta caratê? <br> Porque ele luta capoeira",
-]
-
-
-@app.route("/api/joke")
-def joke():
-    return jsonify({"joke": random.choice(joke_list)})
-
-
-@app.route("/api/users/random")
-def users_random():
-    user = requests.get("https://random-data-api.com/api/v2/users").json()
-    print(user)
-    return user
+app.register_blueprint(jokes_controller, url_prefix="/jokes")
 
 
 def start_server(host: str = "0.0.0.0", port: int = 8000):
