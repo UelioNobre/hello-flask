ed7ff6df181ddf5e9cc533e3fc4e0779d771fb3c
uelionobre <uelionobre@hotmail.com>
2023-12-24 10:33:02 -0300

    Implementa rota para salvar uma nova música

--- src/controllers/musics_controller.py
+++ src/controllers/musics_controller.py
@@ -1,5 +1,5 @@
 from bson import ObjectId
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 from models.music_model import MusicModel
 
@@ -33,3 +33,10 @@ def music_random():
     if music is None:
         return
     return jsonify(music.to_dict()), 200
+
+
+@musics_controller.route("/", methods=["POST"])
+def music_post():
+    new_music = MusicModel(request.json)
+    new_music.save()
+    return jsonify(new_music.to_dict()), 201
